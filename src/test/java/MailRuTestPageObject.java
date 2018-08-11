945a55af13110924e8b6b8a5668ae4a14b72712a
Ekaterina Moldavskaia <Ekaterina_Moldavskaia@epam.com>
2018-08-11 23:54:21 +0300

    Add RemoteWebDriver

--- src/test/java/MailRuTestPageObject.java
+++ src/test/java/MailRuTestPageObject.java
@@ -1,12 +1,16 @@
 import PageObjects.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static PageObjects.BaseAreasPage.MAIL_SUBJECT;
@@ -14,12 +18,15 @@ import static org.testng.Assert.*;
 
 public class MailRuTestPageObject {
 
-    private WebDriver driver;
+    private WebDriver driver = null;
 
     @BeforeClass
     public void beforeClass() {
-        System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
-        driver = new ChromeDriver();
+        try {
+            driver = new RemoteWebDriver(new URL("http://192.168.56.1:4444/wd/hub"), DesiredCapabilities.chrome());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
